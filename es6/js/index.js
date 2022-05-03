b78fdc7668e315f0a5843267550ac65f04c53f6c
Joel Ross <joelross@uw.edu>
2022-05-03 10:18:43 -0700

    End of lectyure

--- es6/js/index.js
+++ es6/js/index.js
@@ -1,12 +1,68 @@
 'use strict';
 
-const peopleArray = [
-    {name: 'Ada', height: 64, weight: 135},
-    {name: 'Bob', height: 74, weight: 156, pronoun: 'they/them'},
-    {name: 'Chris', height: 69, weight: 139},
-    {name: 'Diya', height: 69, weight: 144},
-    {name: 'Emma', height: 71, weight: 152}
-]
+import { peopleArray } from './other.js';
+import * as otherModule from './other.js';
+import greet from './other.js';
+
+console.log("index");
+console.log("index", peopleArray);
+//helloOther();
+greet();
+
+// console.log(otherModule.helloOther());
+
+
+
+const foo = (params) => 'foo ' + params;
+
+// console.log(foo("hello"));
+
+
+const myObject = { a: 1, b: 2, c: 3, d: 4 };
+
+// const {a, z} = myObject; //myObject.a to a, etc.
+// // const a = myObject.a
+// // const b = myObject.b
+// // const c = myObject.c
+// console.log('a', a); //=> 1
+// console.log('z', z); //z const = myObject.z
+// // console.log('b', b); //=> 2;
+// //console.log('c', c); //=> 3;
+
+// const a, b, c = [1,2,3];
+
+
+// const person = { name: 'Ada', height: 64 }
+
+//a function that expects a person object
+// function getBMI({ height, weight }) {
+
+//   console.log(height);
+//   console.log(weight);
+
+//   return 703 * weight / (height * height);
+// }
+
+// const person = {name: 'Ada', height: 64, weight: 135}
+
+// console.log( {a: 1, a: 2} )
+
+// const personWithHat = {hat: 'bowler', height: 80, ...person};
+// //const olderAda = {...person, age: person.age+1}
+// console.log(person); //has name, height, weight
+// console.log(personWithHat); //has name, height, weight, hat
+
+
+// //const greet = ({name}) => "hello"+name;
+
+
+
+
+
+
+
+
+
 
 
 
--- es6/js/other.js
+++ es6/js/other.js
@@ -1,1 +1,15 @@
 'use strict';
+
+console.log("other");
+
+export const peopleArray = [
+    { name: 'Ada', height: 64, weight: 135 },
+    { name: 'Bob', height: 74, weight: 156, pronoun: 'they/them' },
+    { name: 'Chris', height: 69, weight: 139 },
+    { name: 'Diya', height: 69, weight: 144 },
+    { name: 'Emma', height: 71, weight: 152 }
+  ]
+  
+export default function helloOther() {
+    console.log("hello other");
+}

805a9b451163b82ffbc8166ec841a5e347654d69
Joel Ross <joelross@uw.edu>
2022-05-09 13:39:07 -0700

    Add starter for chat demo

---
+++ react-chat/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+//css!
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+
+import App from './components/App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
